99aa23f23810eb9e4e6f3bc8ba3e05852440f13f
felixsiu <b1044327634@gmail.com>
2023-04-14 17:05:07 +0800

    amend

--- src/Pages/Search/Search.jsx
+++ src/Pages/Search/Search.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
 import Input from './Input/Input'
 import List from './List/List'
+import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
 export default function Search() {
   return (
     <div className='mt-5'>
-      <Row>
-          <Col xs={11} md={9} lg={8} xl={6} className='mx-auto'>
-            <Input></Input>
-          </Col>
-      </Row>
-      <Row>
-          <Col xs={11} md={10} lg={9} xl={7} className='mx-auto'>
-            <List></List>
-          </Col>
-      </Row>
+      <Container>
+        <Row>
+            <Col xs={11} md={9} lg={8} xl={6} className='mx-auto'>
+              <Input></Input>
+            </Col>
+        </Row>
+        <Row>
+            <Col xs={11} md={10} lg={9} xl={7} className='mx-auto'>
+              <List></List>
+            </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
